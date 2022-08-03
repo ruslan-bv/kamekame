970ec71193c0d7ceaada4025f36c6da5717dcd4e
Ruslan <seitensogekihei@protonmail.ch>
2022-08-03 13:15:32 +0500

    tiny refactoring for app module and mongo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,20 +2,20 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path/posix';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { secret } from './utils/constants';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:3000/'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/app'),
     JwtModule.register({
       secret,
-      signOptions: { expiresIn: '2h' },
+      signOptions: { expiresIn: '1h' },
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
+      rootPath: join(__dirname, '..', 'dist'),
     })
   ],
   controllers: [AppController],
