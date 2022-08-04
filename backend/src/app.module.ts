63ad52d1755fe8b061852e427a6f5d95cef7f170
Ruslan <seitensogekihei@protonmail.ch>
2022-08-04 14:26:52 +0500

    moved JwTmodule to usermodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JwtModule } from '@nestjs/jwt';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { secret } from './utils/constants';
 import { UserModule } from './modules/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/app'),
-    JwtModule.register({
-      secret,
-      signOptions: { expiresIn: '1h' },
-    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'dist'),
     }),
--- backend/src/modules/user.module.ts
+++ backend/src/modules/user.module.ts
@@ -4,11 +4,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserController } from '../controllers/user.controller';
 import { UserService } from '../services/user.service';
 import { User, UserSchema } from '../models/user.schema';
+import { secret } from '../utils/constants';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), JwtModule],
+    imports: [
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), 
+        JwtModule.register({
+            secret,
+            signOptions: { expiresIn: '1h' },
+          }),
+    ],
     controllers: [UserController],
-    providers: [UserService]
+    providers: [UserService],
+    exports: [JwtModule]
 })
 
 export class UserModule {}
