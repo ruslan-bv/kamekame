be4c963bd99730829b0abcfa5d930410d1fed121
Ruslan <seitensogekihei@protonmail.ch>
2022-08-31 14:38:15 +0500

    added React Router for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Account from './pages/Account';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kamekame!</h1>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/pages/Account.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Account:React.FC = () => {
+    const title = 'Account management';
+
+    return (
+        <div>
+            <h2>{title}</h2>
+        </div>
+    )
+}
+
+export default Account;
