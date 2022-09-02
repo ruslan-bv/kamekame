b6d110322012a4879cabbc3ed435051dba9d44db
Ruslan <seitensogekihei@protonmail.ch>
2022-09-02 18:12:22 +0500

    added header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { AppBar, Box, Button, ButtonGroup, Toolbar, Typography, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const Header:React.FC = () => {
+    return (
+        <AppBar>
+            <Toolbar sx = {{ display: 'flex', justifyContent: 'space-between' }}>
+                <IconButton color="inherit" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h4">
+                    カメカメ：Your Kanji Wordbook
+                </Typography>
+                <ButtonGroup variant="contained">
+                    <Button>Login</Button>
+                    <Button>Register</Button>
+                </ButtonGroup>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header;
