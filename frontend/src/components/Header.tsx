86c1a6aa98a3dd7b4713684461dd72b0238a408d
Ruslan <seitensogekihei@protonmail.ch>
2022-09-06 19:14:11 +0500

    added router for menu burger

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { AppBar, Box, Button, ButtonGroup, Toolbar, Typography, IconButton } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { AppBar, Box, Button, ButtonGroup, Toolbar, Typography, IconButton,
+         Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 interface HeaderProps {
@@ -7,12 +9,30 @@ interface HeaderProps {
 }
 
 const Header:React.FC<HeaderProps> = ({ isAccountOn }) => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <AppBar position="static">
             <Toolbar sx = {{ display: 'flex', justifyContent: 'space-between' }}>
-                <IconButton color="inherit" sx={{ mr: 2 }}>
+                <IconButton onClick={handleClick} color="inherit" sx={{ mr: 2 }}>
                     <MenuIcon />
                 </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                >   
+                    <MenuItem component={Link} to={'/'}>Home</MenuItem>
+                    <MenuItem component={Link} to={'/account'}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
                 <Typography variant="h4">
                     カメカメ：Your Kanji Wordbook
                 </Typography>
--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, Toolbar, Typography, IconButton, TextField } from '@mui/material';
+import { Box, Button, ButtonGroup, Toolbar, Typography, IconButton, TextField } from '@mui/material';
 import Header from '../components/Header';
 
 
@@ -19,16 +19,26 @@ const Account:React.FC = () => {
                 // width: 200,
                 height: 400,
             }}>
-                <Typography color="white" variant="h4">Your Account</Typography>
+                <Typography color="white" variant="h4">Account Control</Typography>
                 <Box component="form" sx={{ 
                            display: 'flex',
-                           justifyContent: 'space-around',
+                           flexDirection: 'column',
+                           justifyContent: 'center',
+                           alignItems: 'center',
                            padding: 2, 
                            backgroundColor: 'primary.light' 
                         }}>
                     <TextField label="Your name" variant="outlined"></TextField>
                     <TextField label="Your email" variant="outlined"></TextField>
+                    <TextField label="Your password" variant="outlined"></TextField>
+                    <TextField label="Number of decks" variant="outlined"></TextField>
                 </Box>
+                <ButtonGroup sx={{
+                    mt: 5
+                }} variant="contained">
+                    <Button color="info">Save</Button>
+                    <Button color="warning">Delete Account</Button>
+                </ButtonGroup>
             </Box>
         </div>
     )
