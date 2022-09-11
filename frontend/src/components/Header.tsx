d4e0064997150b1cb92ca5b6ba0e6ab0133eb301
Ruslan <seitensogekihei@protonmail.ch>
2022-09-11 11:01:18 +0500

    added login page

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Box, Button, ButtonGroup, Toolbar, Typography, IconButton,
-         Menu, MenuItem } from '@mui/material';
+         Menu, MenuItem, Modal } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import Login from '../pages/Login';
 
 interface HeaderProps {
     isAccountOn?: boolean;
@@ -17,6 +18,10 @@ const Header:React.FC<HeaderProps> = ({ isAccountOn }) => {
     const handleClose = () => {
         setAnchorEl(null);
     };
+
+    const [modal, setModal] = React.useState(false);
+    const handleOpenModal = () => setModal(true);
+    const handleCloseModal = () => setModal(false);
 
     return (
         <AppBar position="static">
@@ -39,7 +44,10 @@ const Header:React.FC<HeaderProps> = ({ isAccountOn }) => {
                 {
                  !isAccountOn ?
                  <ButtonGroup variant="contained">
-                    <Button>Login</Button>
+                    <Button onClick={handleOpenModal}>Login</Button>
+                    <Modal open={modal} onClose={handleCloseModal}>
+                        <Login />
+                    </Modal>
                     <Button href="/register">Register</Button>
                  </ButtonGroup> :
                  <Button variant="contained">Logout</Button>   
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Box, Button, Typography, TextField } from '@mui/material';
+
+const Login:React.FC = () => {
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+    
+    const [login, setLogin] = React.useState({
+        email: '',
+        password: ''
+    });
+
+    const onChangeLogin = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setLogin((prevLogin) => {
+            return {
+                ...prevLogin, [name]: value
+            }
+        });
+    }
+    
+    return (
+        <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+            Create Kamekame Account
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ 
+                mt: 2, 
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+            <TextField 
+                name="email" 
+                onChange={onChangeLogin} 
+                label="Your email" 
+                variant="outlined"
+            >{login.email}
+            </TextField>
+            <TextField 
+                name="password" 
+                onChange={onChangeLogin} 
+                label="Your password" 
+                type="password" 
+                variant="outlined"
+            >{login.password}
+            </TextField>
+            <Button color="info">Sign in</Button>
+            </Typography>
+      </Box>
+    )
+}
+
+export default Login;
