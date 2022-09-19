76ddaa7a36e90dd6af79d24fa89998f617fba411
Ruslan <seitensogekihei@protonmail.ch>
2022-09-19 11:22:25 +0500

    added deck creation modal

--- frontend/src/pages/Decks.tsx
+++ frontend/src/pages/Decks.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Container, Card, CardContent, CardActions, Button, Typography } from '@mui/material';
+import { Box, Container, Card, CardContent, CardActions, Button, TextField, Typography, Modal } from '@mui/material';
 import Header from '../components/Header';
 import Deck from '../components/Deck';
 import { UserContext, UserContextInterface } from '../context/context';
@@ -8,8 +8,45 @@ import { UserContext, UserContextInterface } from '../context/context';
 // }
 
 const Decks:React.FC = () => {
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+
     const { email } = React.useContext(UserContext) as UserContextInterface;
     const decks = [{name: 'animals'}, {name: 'prefectures'}, {name: 'cities'}];
+
+    const [newDeck, setNewDeck] = React.useState('');
+    const onChangeNewDeck = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const { value } = e.target;
+        setNewDeck(value);
+    }
+
+    const [modal, setModal] = React.useState(false);
+    const handleOpenModal = () => setModal(true); 
+    const handleCloseModal = () => setModal(false);
+
+    const deckCreation = (
+        <Box sx={style}>
+            <Typography variant="h6">Start a new deck</Typography>
+            <TextField
+                name="deck"
+                onChange={onChangeNewDeck}
+                label="Deck name"
+                variant="outlined"
+            >
+                {newDeck}
+            </TextField>
+            <Button>Save</Button>
+        </Box>
+    )
 
     return (
         <div>
@@ -24,6 +61,10 @@ const Decks:React.FC = () => {
                     return <span>{deck.name}</span>
                 })}
             </Box>
+            <Button onClick={handleOpenModal}>Start a new deck</Button>
+            <Modal open={modal} onClose={handleCloseModal}>
+                {deckCreation}
+            </Modal>
         </div>
     )
 }
